ed492a2fc1b8a573e5218b6b23b9b406b4c797ce
ATHULBIJU123 <ak.athulbijukumar@gmail.com>
2024-04-22 09:00:46 +0530

    April22: React-Count

--- REACT/vite-project/src/App.jsx
+++ REACT/vite-project/src/App.jsx
@@ -1,13 +1,17 @@
 import { Fragment } from "react";
-import Greeting from "./components/Greeting"
-import Greeting1 from "./components/Greeting1";
+// import Greeting from "./components/Greeting"
+// import Greeting1 from "./components/Greeting1";
+import Counter from "./components/Counter";
+import Counter1 from "./components/Counter1";
 function App() {
 
 
   return (
     <>
-    <Greeting name={"Alan"}/>
-    <Greeting1 name = {"Mark"} age= {15} />
+    {/* <Greeting name={"Alan"}/>
+    <Greeting1 name = {"Mark"} age= {15} /> */}
+    {/* <Counter/> */}
+    <Counter1/>
     </>
   )
 }
